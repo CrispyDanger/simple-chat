de7111564bc9ea66be959e7d249423f53b0a79f8
CrispyDanger <crispyd2405@gmail.com>
2023-04-11 18:16:40 +0500

    Add models to chat app

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message, Thread
 
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender", "thread", "text")
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Thread)
 # Register your models here.
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Thread(BaseModel):
+    participants = models.ManyToManyField(User, verbose_name=("participants"))
+
+    def add_participant(self, participant):
+        if self.participants.count() >= 2:
+            raise Exception("Too many participants")
+        else:
+            self.participants.add(participant)
+
+    def __str__(self) -> str:
+        users = list(self.participants.all().values_list("username", flat=True))
+        return f"{users[0]} and {users[1]} Chat"
+
+
+class Message(BaseModel):
+    sender = models.OneToOneField(
+        User, verbose_name=("sender"), on_delete=models.CASCADE
+    )
+    text = models.TextField(("text"), null=False, max_length=50)
+    thread = models.OneToOneField(
+        Thread, verbose_name=("thread"), on_delete=models.CASCADE
+    )
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.sender} message in "{self.thread}"'
+
 
 # Create your models here.
